60928c71a5710db3447941c1d3e40b38b03eb755
Dave Coate <coateds@outlook.com>
2021-08-21 19:42:58 -0700

    change path to psm1 and and get-command test

--- Tests/ServerCollectionToolsModule.Tests.ps1
+++ Tests/ServerCollectionToolsModule.Tests.ps1
@@ -1,9 +1,14 @@
 Describe 'Module-Level tests' {
     BeforeAll {
-        Import-Module "$PSScriptRoot\ServerCollectionToolsModule.psm1" -Force
+        Import-Module "$PSScriptRoot\..\ServerCollectionToolsModule.psm1" -Force
     } 
 
     It 'Runs a simple test to verify Pester works' {
         $false | Should -Be $false
+    }
+
+    it 'the module imports successfully' {
+        #{ Import-Module "$PSScriptRoot\PowerShellModuleProject.psm1" } | should -not throw
+        (Get-Command Get-ServerObjectCollection ).Source | Should -Be ServerCollectionToolsModule
     }
 }
