a4f031fc041fd5a822af3be17a3c8575115d7efd
Dave Coate <coateds@outlook.com>
2021-08-24 11:15:06 -0700

    expanded server connection tests

--- Tests/FunctionTest-ServerConnectionOnPipeline.Tests.ps1
+++ Tests/FunctionTest-ServerConnectionOnPipeline.Tests.ps1
@@ -2,35 +2,24 @@ Describe 'Test-ServerConnectionOnPipeline.Tests' {
     Context 'Output test - Ping Fail' {
         BeforeAll {
             Import-Module "$PSScriptRoot\..\ServerCollectionToolsModule.psm1" -Force
-            
+            Mock -CommandName Test-Connection -ModuleName ServerCollectionToolsModule -MockWith {
+                return $false
+            }            
             $Actual = ('Server1', 'Server2') | Get-ServerObjectCollection | 
-            Test-ServerConnectionOnPipeline
-
-            $Actual
-        }
-
-        Mock -CommandName Test-Connection -ModuleName ServerCollectionTools -MockWith {
-            return $false
+                Test-ServerConnectionOnPipeline
+            $MemberArray = $Actual | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+            $MemberArray
         }
 
         It 'Should return a custom object' {
             $Actual | Should -BeOfType PSCustomObject
         }
 
-        #It 'Should have 4 new properties' {
-        #    $TestDrive
-        #    $TestFile = "out.csv"
-        #    $Actual | export-csv "$($TestDrive)\$($TestFile)"
-        #    $FirstLine = Get-Content "$($TestDrive)\$($TestFile)"
-        #    $ActualProperties = $FirstLine[1].Split(',')
-        #    $ActualProperties | Should -Contain '"Ping"'
-        #    $ActualProperties | Should -Contain '"WMI"'
-        #    $ActualProperties | Should -Contain '"PSRemote"'
-        #    $ActualProperties | Should -Contain '"BootTime"'
-        #}
-
-        It 'Should have Ping Property' {
-            ($null -ne $Actual.Pings) | Should -Be $true
+        It 'Should have 4 new properties' {
+            $MemberArray | Should -Contain 'Ping'
+            $MemberArray | Should -Contain 'WMI'
+            $MemberArray | Should -Contain 'PSRemote'
+            $MemberArray | Should -Contain 'BootTime'
         }
 
         It 'Ping should have failed' {
@@ -38,6 +27,36 @@ Describe 'Test-ServerConnectionOnPipeline.Tests' {
             $Actual[0].WMI | should -be $null
             $Actual[0].PSRemote | should -be $null
             $Actual[0].BootTime | should -be $null
+        }
+    }
+
+    Context 'Output test - Ping Success, WMI Fail, PSRemote Fail' {
+        BeforeAll {
+            Import-Module "$PSScriptRoot\..\ServerCollectionToolsModule.psm1" -Force
+
+            Mock -CommandName Test-Connection -ModuleName ServerCollectionToolsModule -MockWith {
+                return $true
+            }
+    
+            Mock -CommandName Get-WMI_OS -ModuleName ServerCollectionToolsModule -MockWith {
+                return $null
+            }
+    
+            Mock -CommandName Get-PSRemoteComputerName -ModuleName ServerCollectionToolsModule -MockWith {
+                return $null
+            }
+            
+            $Actual = ('Server1', 'Server2') | Get-ServerObjectCollection | 
+                Test-ServerConnectionOnPipeline
+            $MemberArray = $Actual | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+            $MemberArray
+        }
+
+        It 'Ping, but not WMI or PSRemote' {
+            $Actual[0].ping | should -be $true
+            $Actual[0].WMI | should -be $false
+            $Actual[0].PSRemote | should -be $false
+            $Actual[0].BootTime | should -be 'No Try'
         }
     }
 }
